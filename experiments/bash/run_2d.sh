bd75724f26f37f83c4b9cd8d5f389b857de23532
Junyu-Liu-Nate <ljunyu381@gmail.com>
2024-07-19 13:16:29 -0400

    Add scripts for stress test

--- experiments/bash/run_2d.sh
+++ experiments/bash/run_2d.sh
@@ -12,16 +12,35 @@
 #               -o ../experiments/output/contact-2D-higher-order/walker-falling/
 
 #========== Quad mesh
-### Unit quad mesh (3*3)
-./PolyFEM_bin -j /Users/liujunyu/Desktop/Research/UVic_NYU/IGA_IPC/code/polyfem/experiments/json/iga-ipc/unit-quad-falling.json \
-              -o ../experiments/output/contact-2D-IGA/unit-quad-falling/
+##### Unit quad mesh (3*3)
+# ./PolyFEM_bin -j /Users/liujunyu/Desktop/Research/UVic_NYU/IGA_IPC/code/polyfem/experiments/json/iga-ipc/unit-quad-falling.json \
+#               -o ../experiments/output/contact-2D-IGA/unit-quad-falling/
 
 # ./PolyFEM_bin -j /Users/liujunyu/Desktop/Research/UVic_NYU/IGA_IPC/code/polyfem/experiments/json/iga-ipc/unit-quad-horizon.json \
 #               -o ../experiments/output/contact-2D-IGA/unit-quad-horizon/
 
-### L shape quad mesh
+##### L shape quad mesh
 # ./PolyFEM_bin -j /Users/liujunyu/Desktop/Research/UVic_NYU/IGA_IPC/code/polyfem/experiments/json/iga-ipc/L-shape-quad-falling.json \
 #               -o ../experiments/output/contact-2D-IGA/L-shape-quad-falling/
 
 # ./PolyFEM_bin -j /Users/liujunyu/Desktop/Research/UVic_NYU/IGA_IPC/code/polyfem/experiments/json/iga-ipc/L-shape-quad-horizon.json \
 #               -o ../experiments/output/contact-2D-IGA/L-shape-quad-horizon/
+
+##### Trash Compactor
+# ./PolyFEM_bin -j /Users/liujunyu/Desktop/Research/UVic_NYU/IGA_IPC/code/polyfem/experiments/json/iga-ipc/trash-compactor-unit-quads.json \
+#               -o ../experiments/output/contact-2D-IGA/trash-compactor-unit-quads/
+
+./PolyFEM_bin -j /Users/liujunyu/Desktop/Research/UVic_NYU/IGA_IPC/code/polyfem/experiments/json/iga-ipc/trash-compactor-L-shapes.json \
+              -o ../experiments/output/contact-2D-IGA/trash-compactor-L-shapes/
+
+##### Funnel
+# ./PolyFEM_bin -j /Users/liujunyu/Desktop/Research/UVic_NYU/IGA_IPC/code/polyfem/experiments/json/iga-ipc/funnel-unit-quad.json \
+#               -o ../experiments/output/contact-2D-IGA/funnel-unit-quad/
+
+# ./PolyFEM_bin -j /Users/liujunyu/Desktop/Research/UVic_NYU/IGA_IPC/code/polyfem/experiments/json/iga-ipc/funnel-L-shape.json \
+#               -o ../experiments/output/contact-2D-IGA/funnel-L-shape/
+
+
+##### Penguin quad mesh
+# ./PolyFEM_bin -j /Users/liujunyu/Desktop/Research/UVic_NYU/IGA_IPC/code/polyfem/experiments/json/iga-ipc/penguin-quad-falling.json \
+#               -o ../experiments/output/contact-2D-IGA/penguin-quad-falling/
